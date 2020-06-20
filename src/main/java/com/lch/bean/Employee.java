4112aee2261c2d150653ad7a92c22dfecf211c00
Coin224 <821504961@qq.com>
2020-06-20 10:27:02 +0800

    Initial commit

---
+++ src/main/java/com/lch/bean/Employee.java
@@ -0,0 +1,10 @@
+package com.lch.bean;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private String address;
+}
